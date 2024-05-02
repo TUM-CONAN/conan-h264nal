f0740a96929c92b0cb090f2a6b4bdd69692d517b
Ulrich Eck <ulrich.eck@tum.de>
2024-05-02 18:54:24 +0200

    Update conanfile.py
    try to compile on windows using msys2

--- conanfile.py
+++ conanfile.py
@@ -31,7 +31,11 @@ class H264nalConan(ConanFile):
     def requirements(self):
         self.requires("gtest/1.14.0")
 
-
+    def build_requirements(self):
+        if self._settings_build.os == "Windows":
+            self.win_bash = True
+            if not self.conf.get("tools.microsoft.bash:path", check_type=str):
+                self.tool_requires("msys2/cci.latest")
 
     def config_options(self):
         if self.settings.os == 'Windows':
